9852559f2ab1ef10e4eab78c8ae440cbc6150da6
yaminibandi <33190226+yaminibandi@users.noreply.github.com>
2020-02-24 11:06:45 +0530

    Update Catalogue.java

--- src/main/java/constructionCost/Catalogue.java
+++ src/main/java/constructionCost/Catalogue.java
@@ -9,9 +9,9 @@ public class Catalogue {
 	
 	public void addCatalogue(){
 		items.put("standard materials", 1200);
-		items.put("above standard materials", 1200);
-		items.put("high standard materials", 1200);
-		items.put("high standard materials and fully automated", 1200);
+		items.put("above standard materials", 1500);
+		items.put("high standard materials", 1800);
+		items.put("high standard materials and fully automated", 2500);
 	}
 	
 	public Map<String, Integer> getCatalogue() {
